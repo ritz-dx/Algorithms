7a0ee33cd12099b1e4ab08a0180b8552b9600709
Ritu Harivansh Prajapati <45092679+ritz-dx@users.noreply.github.com>
2025-08-12 19:33:03 +0530

    Time: 1 ms (68.62%) | Memory: 17.9 MB (32.42%) - LeetSync

---
+++ BinarySearch/LeetCodeProblems/69-sqrtx/sqrtx.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        if x < 2:
+            return x
+
+        low = 1
+        high = x
+              
+        while  low <= high:
+            mid = (low + high) // 2
+
+            if mid*mid <= x:
+                low = mid + 1
+            else :
+                high = mid - 1
+        return high
+           
+
+
+        
